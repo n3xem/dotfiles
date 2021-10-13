162ef30608d736b9083c5f83a6e85dbdf6cb8f51
Yukyan <yukyanjpn@gmail.com>
2021-10-13 20:54:24 +0900

    初期設定ファイルを追加

---
+++ init.sh
@@ -0,0 +1,5 @@
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.bash_aliases ~/.bash_aliases
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.inputrc ~/.inputrc
