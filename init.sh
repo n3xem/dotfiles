5403e139cbe69eba09c04987879b34b4ffcd4e32
Yukyan <yukyanjpn@gmail.com>
2021-12-17 14:58:36 +0900

    .gitincludeをりぽじとりから除外

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@ ln -sf ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -sf ~/dotfiles/.bash_private ~/.bash_private
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitinclude ~/.gitinclude
 ln -sf ~/dotfiles/.inputrc ~/.inputrc
