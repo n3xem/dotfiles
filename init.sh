c6e5106bdb9ea9ba8a91a79c6cbfc618a45fc8d5
yukyan <yukyan@pepabo.com>
2022-06-14 15:43:38 +0900

    .tmux.conf

--- init.sh
+++ init.sh
@@ -8,3 +8,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitinclude ~/.gitinclude
 ln -sf ~/dotfiles/.inputrc ~/.inputrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
